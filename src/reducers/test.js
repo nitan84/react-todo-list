6f995776945a35d338da7e7f266ce1b01f26c942
nitan84 <ntan804@gmail.com>
2018-05-16 22:30:01 +0800

    Added reducers

---
+++ src/reducers/test.js
@@ -0,0 +1,33 @@
+/* global expect, it, describe */
+
+import { reducer, initialState } from '.';
+import types from '../constants';
+
+describe('Reducer', () => {
+  const todoText = 'A todo';
+
+  it('Should return the initial state when no action passed', () => {
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  describe('Submit todo', () => {
+    it('Should return the correct state', () => {
+      const action = {
+        type: types.SUBMIT_TODO,
+        id: 1,
+        text: todoText,
+      };
+
+      const expectedState = {
+        todos: [
+          {
+            id: 1,
+            text: todoText,
+          },
+        ],
+      };
+
+      expect(reducer(undefined, action)).toEqual(expectedState);
+    });
+  });
+});
